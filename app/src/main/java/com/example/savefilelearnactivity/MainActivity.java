9809825bcbc5109ac1016c0fcda0d8590ade0198
Thomas-Wei-666 <72587206+Thomas-Wei-666@users.noreply.github.com>
2021-04-15 15:17:29 +0800

    v3.0 解决了之前一直写不了文件的问题 原来是因为editText的getText()方法必须放在“监听事件“里面才会返回正常结果，不然一直返回空。。。

--- app/src/main/java/com/example/savefilelearnactivity/MainActivity.java
+++ app/src/main/java/com/example/savefilelearnactivity/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.savefilelearnactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -23,10 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
     private String text;
 
+    private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-            saveText(text);
+        saveText(text);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(MainActivity.this,"you've rejected us",Toast.LENGTH_SHORT).show();
+            }
+            break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -34,24 +54,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         EditText editText = (EditText) findViewById(R.id.et_input_text);
         Button button = (Button) findViewById(R.id.bt_save);
 
-        text = editText.getText().toString();
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        }
 
         button.setOnClickListener(v -> {
-                saveText(text);
+            text = editText.getText().toString();//editText的getText()方法必须放在“监听事件“里面才会返回正常结果，不然一直返回空
+            saveText(text);
+            System.out.println(text);
+
         });
+
+
 
 
     }
 
+
     private void saveText(String text) {
         try {
-        fileOutputStream = openFileOutput("myfile.txt", MODE_PRIVATE);
-        outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-        writer = new BufferedWriter(outputStreamWriter);
-        writer.write(text);
+            fileOutputStream = getApplicationContext().openFileOutput("mydamnfile.txt",MODE_PRIVATE);
+            outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            writer = new BufferedWriter(outputStreamWriter);
+            writer.write(text);
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -62,7 +94,10 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Log.i("write","success");
+            Log.i("write", "success");
         }
     }
+
+
 }
+
